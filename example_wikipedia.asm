a312bf00d3dd08643ce903c1829c3c9b93e77d81
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-04-26 11:02:20 +0200

    Add macro read

--- example_wikipedia.asm
+++ example_wikipedia.asm
@@ -2,12 +2,23 @@ BITS 64
 CPU X64
 
 
+%define stdin 0
 %define stdout 1
 %define syscall_write 0x2000004
+%define syscall_read 0x2000003
 
 ; Analog to write(2): ssize_t write(int fildes, const void *buf, size_t nbyte);
 %macro write 3
     mov rax, syscall_write
+    mov rdi, %1
+    mov rsi, %2
+    mov rdx, %3
+    syscall
+%endmacro
+
+; Analog to read(2): ssize_t read(int fildes, void *buf, size_t nbyte)
+%macro read 3
+    mov rax, syscall_read
     mov rdi, %1
     mov rsi, %2
     mov rdx, %3
@@ -30,22 +41,14 @@ section .bss
 section .text
 
 _start:
-
         ; show prompt
         write stdout, query_string, query_string_len
 
 	; read in the character
-	mov	rax, 0x2000003		; read system call
-	mov	rdi, 0				; stdin
-	mov	rsi, in_char		; address for storage, declared in section .bss
-	mov	rdx, 2				; get 2 bytes from the kernel's buffer (one for the carriage return)
-	syscall
-
-
+        read stdin, in_char, 2 ; get 2 bytes from the kernel's buffer (one for the carriage return)
 	; show user the output
         write stdout, out_string, out_string_len
         write stdout, in_char, 2 ; the second byte is to apply the carriage return expected in the string
-
 
 	; exit system call
 	mov	rax, 0x2000001		; exit system call
