3bc81dfc6d14d89a7c88dbcd97d94e387dd227a4
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-04-27 15:08:08 +0200

    Add call C func example

---
+++ call_c_fun.asm
@@ -0,0 +1,48 @@
+BITS 64
+CPU X64
+DEFAULT REL
+
+extern _printf
+
+%define stdin 0
+%define stdout 1
+%define stderr 2
+%define syscall_exit 0x2000001
+%define syscall_write 0x2000004
+
+%macro exit 1
+    mov rdi, %1
+    mov rax, syscall_exit
+    syscall
+%endmacro
+
+
+section .data
+    err_string: db "Error with syscall"
+    err_string_len: equ $- err_string
+    fmt_string: db "`%d`", 0
+
+section .bss
+
+section .text
+
+global _start
+_start:
+    ; prolog
+    push rbp
+    mov rbp, rsp
+    and rsp, -16
+
+    ; pass args
+    mov rdi, fmt_string
+    mov rsi, 42
+    xor rdx, rdx
+    xor rax, rax
+
+    call _printf
+    xor eax, eax
+    
+    ; epilog
+    mov rsp, rbp
+    pop rbp
+    ret
